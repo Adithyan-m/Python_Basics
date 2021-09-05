bfe3b7a5ac296dd2110529c86ce341238746e990
Adithyan-m <adithyan.v@outlook.com>
2021-09-05 19:28:08 +0400

    after adding translater

--- Basic translater.py
+++ Basic translater.py
@@ -1,1 +1,22 @@
+'''
+vowels get turned into a Z
+This means
+ cat -> czt
+'''
+
+
 def translate(phrase):
+    trans = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":        # to avoids seraching A E I O U (covertes letter to lowercase)
+            if letter.isupper():
+                trans = trans + "z"
+            else:
+                trans = trans + "z"
+        else:
+            trans = trans + letter
+
+    return trans
+
+print(translate(input("Enter a phrase to translate: ")))
+
